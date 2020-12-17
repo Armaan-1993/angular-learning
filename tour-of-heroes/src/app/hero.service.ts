44087c349dac46f47e0ff1ebe313dd036de1cfbf
armaan <armaanmathew88@gmail.com>
2020-12-18 05:19:00 +0530

    working with getting requests from http

--- tour-of-heroes/src/app/hero.service.ts
+++ tour-of-heroes/src/app/hero.service.ts
@@ -4,19 +4,35 @@ import {HEROES} from "./mock-heroes"
 import {Observable, of} from "rxjs";
 //observable is used to return value to Hero[]
 import {MessageService} from "./message.service"
+import {HttpClient ,HttpHeaders} from "@angular/common/http"
+import { catchError, map, tap } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) {
-    
-   }
+  constructor(private messageService: MessageService,
+    private http:HttpClient,
+    ) {}
+
+    private heroesUrl = 'api/heroes'
+
+  private log(message: string) {
+    this.messageService.add (`HeroService: ${message}`)
+  }
+
+  private handleError<T>(operation = 'operation', result?:T) {
+    return (error:any): Observable<T> => {
+      console.log(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of (result as T);
+    }
+  } // doubt in the whiole private function
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes')
-    return of ( HEROES)
-  }
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(catchError(this.handleError<Hero[]>('getHeroes', [])))
+}
   getHero(id :number): Observable<Hero> {
     this.messageService.add(`HeroService: fetched hero id = ${id}`);
     return of (HEROES.find(hero => hero.id === id))
