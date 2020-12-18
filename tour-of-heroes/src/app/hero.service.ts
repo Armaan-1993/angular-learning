8551434610bac63ebb8e065149f17da33ee36a24
armaan <armaanmathew88@gmail.com>
2020-12-18 18:52:09 +0530

    requests from server

--- tour-of-heroes/src/app/hero.service.ts
+++ tour-of-heroes/src/app/hero.service.ts
@@ -7,6 +7,9 @@ import {MessageService} from "./message.service"
 import {HttpClient ,HttpHeaders} from "@angular/common/http"
 import { catchError, map, tap } from 'rxjs/operators'
 
+  //Heroservice fetches the data from HEROES synchronously.
+  //But this cannot be implemented in case of a real backend API
+  //Hence we need to use some asynchronous property to this.
 @Injectable({
   providedIn: 'root'
 })
@@ -21,6 +24,9 @@ export class HeroService {
   private log(message: string) {
     this.messageService.add (`HeroService: ${message}`)
   }
+  httpOptions = {
+    headers: new HttpHeaders ({'Content-Type': 'application/json'})
+  }
 
   private handleError<T>(operation = 'operation', result?:T) {
     return (error:any): Observable<T> => {
@@ -31,13 +37,27 @@ export class HeroService {
   } // doubt in the whiole private function
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes')
-    return this.http.get<Hero[]>(this.heroesUrl).pipe(catchError(this.handleError<Hero[]>('getHeroes', [])))
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap(_ =>this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes', [])))
   }
   getHero(id :number): Observable<Hero> {
-    this.messageService.add(`HeroService: fetched hero id = ${id}`);
-    return of (HEROES.find(hero => hero.id === id))
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ =>this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+    // this.messageService.add(`HeroService: fetched hero id = ${id}`);
+    // return of (HEROES.find(hero => hero.id === id))
   }
-  //Heroservice fetches the data from HEROES synchronously.
-  //But this cannot be implemented in case of a real backend API
-  //Hence we need to use some asynchronous property to this.
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl,hero,this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id = ${hero.id}`)),
+      catchError(this.handleError<any>("updateHero"))
+    )
+  }
+  //The HttpClient.put() method takes three parameters: 
+  //the URL, the data to update (the modified hero in this case) ,options
+
+
 }
--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,3 +1,5 @@
+import { catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { HeroService } from './../hero.service';
 import { Component, OnInit } from '@angular/core';
 import {Hero} from "../hero"
@@ -22,8 +24,29 @@ export class HeroesComponent implements OnInit {
   ngOnInit(): void {
     this.getHeroes(); //life cycle method. Angular will call getHeroes at the required time
   }
+    //to add new hero to the server
+
+    addHero(hero: Hero): Observable<Hero> {
+      return this.http.post<Hero>(this.heroesUrl , hero, this.httpOptions).pipe(
+        tap((newHero: Hero) => this.log(`added hero w/ id = ${newHero.id}`),
+        catchError(this.handleError<Hero>('addHero'))
+      );
+    }
+
+  add(name:string) :void {
+    name = name.trim();
+    if(!name) {
+      return;
+    }
+    this.heroService.addHero({name} as Hero).subscribe(hero => {
+      this.heroes.push(hero)
+    });
+  }
+
+
   // onSelect(hero:Hero): void { //doubt
     // this.selectedHero = hero; 
   //   this.messageService.add("HeroesComponent: Selected Hero")
   // }
 }
+ 
